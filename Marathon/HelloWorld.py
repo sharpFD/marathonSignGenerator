f954b32ecd9efea197348acf188a665d26b61afd
wuyungen <wuyungen@qiyi.com>
2021-10-27 21:02:02 +0800

    [feature] add README

--- Marathon/HelloWorld.py
+++ Marathon/HelloWorld.py
@@ -1,5 +1,10 @@
 import execjs
 file = 'jsCode.js'
 ctx = execjs.compile(open(file, encoding="utf-8").read())
-params = ctx.call('generatorMarathonSign',"appid=5PFuGr3Q7AA0vbJAFior&channel=windows-PC&device_id=Z8T23iCPme3DWGRAQLpJO2w0qY2Kq9-R&language=zh-CN&model=&noncesr=1635235066721&sys_version=&ts=1635235066&version=V1.0","xbbaUe8PPVblLWaVP-iIJnUGhnDXmrm_")
+# 个人参数，按情况修改
+base = 'appid=5PFuGr3Q7AA0vbJAFior&channel=windows-PC&device_id=Z8T23iCPme3DWGRAQLpJO2w0qY2Kq9-R&language=zh-CN&model=&noncesr=1635339630051&sys_version=&ts=1635339630&version=V1.0'
+# 固定校验码，不要改动
+extra = 'xbbaUe8PPVblLWaVP-iIJnUGhnDXmrm_'
+
+params = ctx.call('generatorMarathonSign',base, extra)
 print(params)
---
+++ README.md
@@ -0,0 +1,6 @@
+# 上海马拉松Sign生成代码（Python）
+#使用说明
+## 运行前terminal安装依赖
+`pip install PyExecJS`
+
+### 然后在运行HelloWorld.py即可
